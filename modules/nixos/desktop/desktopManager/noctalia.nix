2baa1b440cc40cc984b8bf600bf44fdca7d1b447
adega <eadegafernandez@gmail.com>
2026-09-25 16:38:51 +0200

    compositor selector for noctalia

--- modules/nixos/desktop/desktopManager/noctalia.nix
+++ modules/nixos/desktop/desktopManager/noctalia.nix
@@ -1,15 +1,31 @@
 {
-  flake.modules.nixos.noctalia = {
-    programs = {
-      niri = {
-        enable = true;
-        useNautilus = true;
+  flake.modules.nixos.noctalia =
+    { config, lib, ... }:
+    {
+      options.noctalia.compositor = lib.mkOption {
+        type = lib.types.enum [
+          "umbriel"
+          "niri"
+        ];
+        default = "niri";
+        description = "The Wayland compositor to use with Noctalia.";
       };
-      noctalia = {
-        enable = true;
-        recommendedServices.enable = true;
-        systemd.enable = true;
+
+      config = {
+        programs = {
+          umbriel.enable = config.noctalia.compositor == "umbriel";
+
+          niri = {
+            enable = config.noctalia.compositor == "niri";
+            useNautilus = true;
+          };
+
+          noctalia = {
+            enable = true;
+            recommendedServices.enable = true;
+            systemd.enable = true;
+          };
+        };
       };
     };
-  };
 }
