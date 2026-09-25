35b32ee4a736790e0ff26664b6ffe69ace2d62dd
adega <eadegafernandez@gmail.com>
2026-09-25 21:17:50 +0200

    nginx

---
+++ modules/nixos/services/nginx.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.nixos.nginx = { config, lib, ... }: {
+    options.nginx = {
+      ports = lib.mkOption {
+        type = lib.types.listOf lib.types.port;
+        description = "List of open ports";
+      };
+    };
+    config = {
+      services.nginx = {
+        enable = true;
+        recommendedGzipSettings = true;
+        recommendedZstdSettings = true;
+        recommendedBrotliSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+        recommendedUwsgiSettings = true;
+      };
+
+      networking.firewall.allowedTCPPorts = config.nginx.ports;
+    };
+  };
+}
